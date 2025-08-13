d56d3c4bb1298bd45d4f96f650525fde744d0055
satvika harivanam <sathvikaharivanam@gmail.com>
2025-08-13 11:08:49 +0530

    this is the user preference page

---
+++ src/pages/preferences.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AnotherHeader from "../components/anotherHeader";
+
+export default function Preferences() {
+  return (
+    <>
+      <AnotherHeader />
+    </>
+  );
+}
