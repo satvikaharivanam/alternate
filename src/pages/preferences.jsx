aeb7be173153ac71d87cab94130b7b487911048f
satvika harivanam <sathvikaharivanam@gmail.com>
2025-08-18 16:41:43 +0530

    added the preferences

--- src/pages/preferences.jsx
+++ src/pages/preferences.jsx
@@ -2,9 +2,57 @@ import React from "react";
 import AnotherHeader from "../components/anotherHeader";
 
 export default function Preferences() {
+  const [preference,setPreference] = React.useState([])
+  const options = [
+    "Fantasy",
+    "Romance",
+    "Science Fiction",
+    "Mystery",
+    "History",
+    "Philosophy",
+    "Horror",
+    "Thriller & Suspense",
+    "Women’s Fiction",
+    "LGBTQ+",
+    "Contemporary Fiction",
+    "Literary Fiction",
+    "Graphic Novel",
+    "Short Story",
+    "Young Adult",
+    "New Adult",
+    "Children’s",
+    "Memoir & Autobiography",
+    "Biography",
+    "Self-help",
+    "True Crime",
+    "Humor",
+    "Religion & Spirituality",
+    "Science & Technology"
+  ]
+  function togglePreference(pref){
+    setPreference((prev) => 
+      prev.includes(pref)
+        ? prev.filter((p) => p !== pref)
+        : [...prev, pref]                
+    );
+  }
   return (
     <>
       <AnotherHeader />
+      <div className="preferencePage">
+        {options.map((opt) => (
+          <button
+            key={opt}
+            onClick={() => togglePreference(opt)}
+            className={`preferenceOption ${
+              preference.includes(opt) ? "selected" : ""
+            }`}
+          >
+            {opt}
+          </button>
+        ))}
+        <button>Done</button>
+      </div>
     </>
   );
 }
