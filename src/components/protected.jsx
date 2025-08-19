334b159a319cd10163fa3094d14aa09f873f4182
satvika harivanam <sathvikaharivanam@gmail.com>
2025-08-19 23:25:06 +0530

    for signup

--- src/components/protected.jsx
+++ src/components/protected.jsx
@@ -1,9 +1,14 @@
 import { Navigate } from "react-router-dom";
 
-export default function protectedRoute({ children }){
+export default function ProtectedRoute({ children, requireLogin = false, requireSignedUp = false }){
   const isLogged = localStorage.getItem("isLogged") === "true"
-  if(!isLogged){
+  const signedUp = localStorage.getItem("signedUp")==="true"
+  if(requireLogin && !isLogged){
     return <Navigate to= "/login" replace/>
+  }
+
+  if(requireSignedUp && !signedUp){
+    return <Navigate to= "/signup" replace/>
   }
 
   return children
